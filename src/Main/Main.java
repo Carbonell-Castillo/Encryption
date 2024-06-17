d7b70936bd4c0c0f72c0868b9e909b4d48003f5f
Carbonell-Castillo <brucecastillo1204@gmail.com>
2024-06-16 22:10:19 -0600

    Finished V1.0

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Logic.FRMencryption;
+
+/**
+ *
+ * @author bccas
+ */
+public class Main {
+    public static void main(String[] args) {
+        FRMencryption initForm = new FRMencryption();
+        initForm.setTitle("Encryption");
+        initForm.setVisible(true);
+    }
+}
